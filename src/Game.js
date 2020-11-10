d20f60ec374dd563330c6c68bff2f5afdd5844a2
isabelchua <isabelchua86@gmail.com>
2020-11-10 13:19:05 -0800

    calculate winner

--- src/Game.js
+++ src/Game.js
@@ -48,7 +48,10 @@ const Board = () => {
 		);
 	};
 
-	const status = `Next player: ${xIsNext ? "X" : "O"}`;
+	const winner = calculateWinner(squares);
+	const status = winner
+		? `Winner: ${winner}`
+		: `Next player: ${xIsNext ? "X" : "O"}`;
 
 	return (
 		<div className="status">
@@ -77,3 +80,29 @@ const Game = () => {
 };
 
 export default Game;
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8], //rows
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8], //columns
+		[0, 4, 8],
+		[2, 4, 6] //diagonals
+	];
+
+	for (let line of lines) {
+		const [a, b, c] = line;
+
+		if (
+			squares[a] &&
+			squares[a] === squares[b] &&
+			squares[a] === squares[c]
+		) {
+			return squares[a]; // 'x' or 'o'
+		}
+	}
+	return null;
+}
